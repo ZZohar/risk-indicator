1c2585d45cb8a0e272f02d1e2bc370bb7e1e3856
ZZohar <zeev.zohar@gmail.com>
2026-02-02 15:14:29 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,13 @@
+import "./globals.css";
+
 export const metadata = {
-  title: 'מדד סיכון',
-  description: 'אינדיקציה כללית למצב',
+  title: "מדד סיכון",
+  description: "אינדיקציה כללית למצב",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="he">
+    <html lang="he" dir="rtl">
       <body>{children}</body>
     </html>
   );
